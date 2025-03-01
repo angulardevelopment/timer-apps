6424a6151e5394a45827a93a14bd25ee510dfb2a
Gupta, Sunny <sunnyvales789@gmail.com>
2025-03-01 20:05:38 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClockComponent } from './clock/clock.component';
+import { DateFormattingComponent } from './date-formatting/date-formatting.component';
+import { NewversionComponent } from './newversion/newversion.component';
+import { CardioComponent } from './pomodoro/cardio.component';
+import { TimeConverterComponent } from './time-converter/time-converter.component';
+import { TimesavingComponent } from './timesaving/timesaving.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ClockComponent', component: ClockComponent},
+  {path: 'DateFormattingComponent', component: DateFormattingComponent},
+  {path: 'NewversionComponent', component: NewversionComponent},
+  {path: 'CardioComponent', component: CardioComponent},
+  {path: 'TimeConverterComponent', component: TimeConverterComponent},
+  {path: 'TimesavingComponent', component: TimesavingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
